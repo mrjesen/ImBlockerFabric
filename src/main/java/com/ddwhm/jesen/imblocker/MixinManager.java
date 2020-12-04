cbaab43912e75d68ddb7c8143ceb3a9a61a56b29
mrjesen <jesen@protonmail.com>
2020-12-04 20:07:23 +0800

    v1.0.10 - Fixed #12.

--- src/main/java/com/ddwhm/jesen/imblocker/MixinManager.java
+++ src/main/java/com/ddwhm/jesen/imblocker/MixinManager.java
@@ -1,14 +1,33 @@
 package com.ddwhm.jesen.imblocker;
 
-import net.minecraft.MinecraftVersion;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 import java.util.Set;
 
 public class MixinManager implements IMixinConfigPlugin {
+    // rewrite the getversion function to fix the issue #12
+    private static int getGameVersion() {
+        try (
+                final InputStream stream = IMixinConfigPlugin.class.getResourceAsStream("/version.json");
+                final Reader reader = new InputStreamReader(stream)
+        ) {
+
+            final JsonObject versions = new JsonParser().parse(reader).getAsJsonObject();
+            return versions.get("protocol_version").getAsInt();
+        } catch (IOException | NullPointerException e) {
+            throw new RuntimeException("[IMBLOCKER] Couldn't get the game protocol_version", e);
+        }
+    }
+
     @Override
     public void onLoad(String mixinPackage) {
 
@@ -21,18 +40,13 @@ public class MixinManager implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        int protocolVersion = MinecraftVersion.create().getProtocolVersion();
+        int protocolVersion = getGameVersion();
         if (!(System.getProperty("os.name").toLowerCase().startsWith("win"))) {
             return false;
         }
-        if (mixinClassName.endsWith("mixin.AbsButtonMixin") && protocolVersion < 705) {
+        if (mixinClassName.endsWith("mixin.AbsButtonMixin") && protocolVersion < 705)
             return false;
-        }
-        if (mixinClassName.endsWith("mixin.AnvilScreenMixin") && protocolVersion < 705) {
-            return false;
-
-        }
-        return true;
+        return !mixinClassName.endsWith("mixin.AnvilScreenMixin") || protocolVersion >= 705;
     }
 
     @Override
