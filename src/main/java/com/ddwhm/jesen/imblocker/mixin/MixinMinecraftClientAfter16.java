acb32bea034f762f7b4b01ab1350920a8cf42a87
Jesen <jesen@protonmail.com>
2022-12-27 14:41:46 +0800

    Fixed crash before 1.16.
    Signed-off-by: Jesen <jesen@protonmail.com>

--- src/main/java/com/ddwhm/jesen/imblocker/mixin/MixinMinecraftClientAfter16.java
+++ src/main/java/com/ddwhm/jesen/imblocker/mixin/MixinMinecraftClientAfter16.java
@@ -10,27 +10,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public abstract class MixinMinecraftClient {
-
-    boolean firstSetScreen = false;
+public abstract class MixinMinecraftClientAfter16 {
 
     @Inject(method = "setScreen", at = @At("HEAD"))
     private void preOpenScreen(Screen screen, CallbackInfo info) {
-        if (!firstSetScreen) {
-            ImBlocker.imManager.makeOff();
-            firstSetScreen = false;
-        }
-
-        if (screen == null) {
-            // 关闭 GUI 时关闭输入法
-            ImBlocker.LOGGER.debug("MixinMinecraftClient.setScreen");
-            WidgetManager.clear();
+        if(screen.getTitle().toString().contains("sign.edit")){
+            ImBlocker.LOGGER.debug("MixinMinecraftClient.sign.edit");
+            WidgetManager.updateWidgetStatus(this, true);
         }
     }
 
-    @Inject(method = "tick", at = @At("HEAD"))
-    private void preTick(CallbackInfo ci) {
-        // 每 tick + 1，超过 1s 没动作的 widget 则移除
-        WidgetManager.tick();
-    }
 }
