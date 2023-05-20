b04321b8a51f56b87a86542ed88dd8342941e4ff
Jesen <jesen@protonmail.com>
2023-05-20 14:07:54 +0800

    fix: keybind failed and cannot input char outside of whitelist.
    修复键盘绑定在某些screen不生效的问题并且变相解决了在mod白名单之外无法输入中文的问题

--- src/main/java/com/ddwhm/jesen/imblocker/ImBlocker.java
+++ src/main/java/com/ddwhm/jesen/imblocker/ImBlocker.java
@@ -6,6 +6,10 @@ import com.ddwhm.jesen.imblocker.immanager.ImManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.apache.commons.lang3.SystemUtils;
@@ -39,6 +43,19 @@ public class ImBlocker implements ClientModInitializer {
     public void onInitializeClient() {
         // 配置键盘绑定
         KeyBinding keyIMBlocker = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.imblocker.switchIMEState", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F6, "category.imblockerfabric.name"));
+        ScreenEvents.AFTER_INIT.register((MinecraftClient client, Screen screen, int scaledWidth, int scaledHeight) -> {
+            if (screen != null) {
+                ScreenKeyboardEvents.afterKeyPress(screen).register((screen1, key, scancode, modifiers) -> {
+                    if (keyIMBlocker.matchesKey(key, scancode)){
+                        if(imManager.getStatus()){
+                            imManager.makeOff();
+                        }else{
+                            imManager.makeOn();
+                        }
+                    }
+                });
+            }
+        });
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (keyIMBlocker.wasPressed()) {
                 if(imManager.getStatus()){
@@ -49,4 +66,5 @@ public class ImBlocker implements ClientModInitializer {
             }
         });
     }
+
 }
